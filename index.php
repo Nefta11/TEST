112378b5806828643f1123549e9a0f94a95b127d
Nefta11 <neftaliarturohernandez@gmail.com>
2024-01-09 18:15:36 -0600

    Update

--- index.php
+++ index.php
@@ -1,36 +1,63 @@
-<?php
-    echo ("Holaa  <br>");
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bienvenido al sistema de estacionamiento</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/estilos.css" rel="stylesheet">
+</head>
+<body>
+    
+<section class="h-100 gradient-form" style="background-color:#eee;">
+<div class="container py-5 h-100">
+    <div class="row d-flex justify-content-center align-items-center h-100">
+        <div class="col-xl-10">
+            <div class="card rounded-3 text-black">
+                <div class="row g-0">
+                    <div class="col-lg-6">
+                        <div class="card-body p-md-5 mx-md-4">
+                            <div class="text-center" >
+                                <img src="imagenes/logo.jpg" style="width: 185px;" alt="logo">
+                                <h4 class="mt-1 mb-5 pg-1">Bienvenido al sistema</h4>
+                            </div>
+                            <form action="">
+                                <p>Ingresa los datos de tu cuenta</p>
+                                <div class="form-outline mb-5">
+                                    <label for="" class="form-label">Usuario</label>
+                                    <input type="email" id="usuario" class="form-control" placeholder="Usuario o correo"/>
+                                </div>
+                                <div class="form-outline mb-5">
+                                <label for="" class="form-label">Contraseña</label>
+                                    <input type="password" id="contraseña" class="form-control" placeholder="Digite su contraseña"/>
+                                </div>
+                                <div class="text-center pt-1 mb-5 pb-1">
+                                    <button class="btn btn-primary btn-block fa-lg gradient-custom-2 mb-3" type="button">Ingresar</button>
+                                    <a href="#" class="text-muted">Recordar Contraseña?</a>
+                                </div>
+                                <div class="d-flex align-items-center justify-content-center ph-4">
+                                    <p class="mb-0 me-2">No tienes Cuenta?</p>
+                                    <button type="Button" class="btn btn-outline-danger">Crear Cuenta</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 d-flex align-items-center gradient-custom-2">
+                        <div class="text-white px-3 py-4 p-md-5 mx-md-4">
+                            <h4 class="mb-4">Bienvenido al sistema de control de estacionamiento</h4>
+                            <p class="small mb-0">
+                            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aliquam delectus sunt atque incidunt quas cumque quis dolores, tempore officia, pariatur nobis minus illum accusamus consectetur ex voluptates dolorum nam! Odit.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</section>
 
-    $nombre="Neftali";
-    $edad= 23;
-    $altura=166.7;
-    $isTrue=true;
-
-    echo $nombre;
-    echo "<br>";
-    echo $edad;
-    echo "<hr>";
-    echo $altura;
-    echo "<br>";
-    echo $isTrue;
-
-    $num1=14;
-    $num2=16;
-    $suma=$num1+$num2;
-    echo "<h1>  El resultado de la suma:".$suma."</h1>";
-
-    if($isTrue==true){
-        echo "<h3>Este valor es trues </h3>";
-    }else{
-        echo "<h3>Este valor es falso </h3>";
-    }
-
-    function resta($num1,$num2){
-        $res=$num1-$num2;
-        echo "<h2>$res</h2>";
-        return $res;
-    }
-
-    resta(4,2);
-
-?>
+    <script src="scripts/js/bootstrap.bundle.min.js">
+    </script>
+</body>
+</html>
