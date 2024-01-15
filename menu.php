ec699a2cb3a9cbc58ecfc0c5f4b61c754635866b
Nefta11 <neftaliarturohernandez@gmail.com>
2024-01-15 17:30:43 -0600

    Update

--- menu.php
+++ menu.php
@@ -4,34 +4,73 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Bienvenido al sistema de estacionamiento</title>
-    <link rel="stylesheet" href="css/bootstrap.min.css">
-    <link rel="stylesheet" href="css/estilos.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/estilos.css"> <!-- Asegúrate de que este archivo esté presente y contenga tus estilos personalizados -->
+    <style>
+        body {
+            font-family: 'Arial', sans-serif;
+            background-color: #f8f9fa;
+            margin: 0;
+            padding: 0;
+        }
+
+        header {
+            /* Estilos de tu clase .gradient-custom-2 */
+            background: #fccb90;
+            background: -webkit-linear-gradient(to right, rgb(213, 120, 223), #36b8d8, #3c36dd, #4547b4);
+            background: linear-gradient(to right, #24ddee, #368ad8, #3660dd, #454bb4);
+            border-top-right-radius: .3rem;
+            border-bottom-right-radius: .3rem;
+            box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.1);
+            padding: 15px 0;
+        }
+
+        .navbar-brand img {
+            width: 55px;
+            height: auto;
+        }
+
+        .navbar-nav .nav-item {
+            margin-right: 15px;
+        }
+
+        .navbar-nav .nav-link {
+            color: #ffffff;
+            font-weight: bold;
+            transition: color 0.3s;
+        }
+
+        .navbar-nav .nav-link:hover {
+            color: #343a40;
+        }
+    </style>
 </head>
 <body>
-    <header>
-        <nav class="navbar navbar-expand-lg navbar-white bg-white px-4">
+    <header class="gradient-custom-2">
+        <nav class="navbar navbar-expand-lg navbar-light bg-light px-4">
             <a class="navbar-brand" href="#">
-                <img src="imagenes/logo.jpg" alt="logo" style="width: 55px;">
+                <img src="imagenes/logo.jpg" alt="logo">
             </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
             <div class="collapse navbar-collapse" id="navbarNavDropdown">
-                <ul class="navbar-nav">
+                <ul class="navbar-nav ml-auto">
                     <li class="nav-item active">
-                        <a class="nav-link text-dark" href="#">Home</a>
+                        <a class="nav-link" href="#">Inicio</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link text-dark" href="#">Features</a>
+                        <a class="nav-link" href="#">Funcionalidades</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link text-dark" href="#">Pricing</a>
+                        <a class="nav-link" href="#">Precios</a>
                     </li>
                 </ul>
             </div>
         </nav>
     </header>
+
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
 </html>
