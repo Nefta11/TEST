0b90a0fd862c25f81eaff36a7d6ab83dd323558f
Nefta11 <neftaliarturohernandez@gmail.com>
2024-01-15 15:41:14 -0600

    UPDATE

--- index.php
+++ index.php
@@ -19,7 +19,7 @@
                                 <div class="card-body p-md-5 mx-md-4">
                                     <div class="text-center">
                                         <img src="imagenes/logo.jpg" alt="logo" style="width: 186px;">
-                                        <h4 class="mt-3 mb-5 pb-1">Bienvenido al sistema SIUUUU</h4>
+                                        <h4 class="mt-3 mb-5 pb-1">Bienvenido al sistema</h4>
                                     </div>
                                     <form action="login.php" method="post">
                                         <p>Ingresa los datos de tu cuenta</p>
