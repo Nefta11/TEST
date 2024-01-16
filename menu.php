0e3787f897d0b6ca80471c0bf469e6980dc842d9
Nefta11 <neftaliarturohernandez@gmail.com>
2024-01-15 18:58:45 -0600

    UPDATE

--- menu.php
+++ menu.php
@@ -1,4 +1,4 @@
-s<?php
+<?php
 session_start();
 if (!isset($_SESSION['loggedin'])){
     echo '<script language="javascript>alert("Tienes que acceder con tu usuario y contraseña");location.href ="index.php";</script>';
