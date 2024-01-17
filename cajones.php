00bb8a65540a64a3a96e1bf0aab0208dd76871d5
Nefta11 <neftaliarturohernandez@gmail.com>
2024-01-16 19:26:38 -0600

    Update

--- cajones.php
+++ cajones.php
@@ -1,29 +1,41 @@
-<h1>Cajon</h1>
-<link rel="stylesheet" href="css/estilos.css">
-<form class="contenedorform" action="login.php" method="post">
-    <p>Ingresa los datos del cajon</p>
-    
-    <div class="form-outline mb-4">
-        <label class="form-label" for="id_cajon">ID_CAJON</label>
-        <select name="id_cajon" id="id_cajon" required>
-            <option value="">Selecciona ID_CAJON</option>
-            <option value="cajon1">Cajon 1</option>
-            <option value="cajon2">Cajon 2</option>
-            <option value="cajon3">Cajon 3</option>
-        </select>
-    </div>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/estilos.css">
+    <title>Registrar uso de estacionamiento</title>
+</head>
+<body>
 
-  
-    <div class="form-outline mb-4">
-        <label class="form-label" for="numero">Número</label>
-        <input type="text" id="numero" name="numero" required>
-    </div>
 
- 
-    <div class="form-outline mb-4">
-        <label class="form-label" for="status">Status</label>
-        <input type="text" id="status" name="status" required>
-    </div>
+    <form class="contenedorform" action="login.php" method="post">
+    <h1>Cajon</h1>
+        <p>Ingresa los datos del cajon</p>
+        
+        <div class="form-outline mb-4">
+            <label class="form-label" for="id_cajon">ID_CAJON</label>
+            <select name="id_cajon" id="id_cajon" required>
+                <option value="">Selecciona ID_CAJON</option>
+                
+                <option value="cajon1">Cajon 1</option>
+                <option value="cajon2">Cajon 2</option>
+                <option value="cajon3">Cajon 3</option>
+            </select>
+        </div>
 
-    <input type="submit" value="Enviar">
-</form>
+        <div class="form-outline mb-4">
+            <label class="form-label" for="numero">Número</label>
+            <input type="text" id="numero" name="numero" required>
+        </div>
+
+        <div class="form-outline mb-4">
+            <label class="form-label" for="status">Status</label>
+            <input type="text" id="status" name="status" required>
+        </div>
+
+        <input type="submit" value="Enviar">
+    </form>
+
+</body>
+</html>
--- clientes.php
+++ clientes.php
@@ -1,1 +1,47 @@
-<h1>Registrar Clientes</h1>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/estilos.css">
+    <title>Registrar Cliente</title>
+</head>
+<body>
+
+
+
+    <form class="contenedorform" action="login.php" method="post">
+    <h1>Cliente</h1>
+        <p>Ingresa los datos del cliente</p>
+        
+
+        <div class="form-outline mb-4">
+            <label class="form-label" for="id_cliente">ID_CLIENTE:</label>
+            <input type="text" id="id_cliente" name="id_cliente" placeholder="ID_CLIENTE" required>
+        </div>
+
+        <div class="form-outline mb-4">
+            <label class="form-label" for="nombre">Nombre:</label>
+            <input type="text" id="nombre" name="nombre" placeholder="Nombre" required>
+        </div>
+
+        <div class="form-outline mb-4">
+            <label class="form-label" for="direccion">Dirección:</label>
+            <input type="text" id="direccion" name="direccion" placeholder="Dirección" required>
+        </div>
+
+        <div class="form-outline mb-4">
+            <label class="form-label" for="telefono">Teléfono:</label>
+            <input type="tel" id="telefono" name="telefono" placeholder="Teléfono" required>
+        </div>
+
+        <div class="form-outline mb-4">
+            <label class="form-label" for="correo">Correo:</label>
+            <input type="email" id="correo" name="correo" placeholder="Correo" required>
+        </div>
+
+        <input type="submit" value="Enviar">
+    </form>
+
+</body>
+</html>
