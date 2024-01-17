51b87191cd47e474700a5bf4b5970804e645817e
Nefta11 <neftaliarturohernandez@gmail.com>
2024-01-17 10:35:56 -0600

    Update

--- scripts/js/funciones.js
+++ scripts/js/funciones.js
@@ -32,4 +32,20 @@ function salir(){
     });
 }
 
+function registrarCliente(){
+    var nombre=$("#nombre").val();
+    var direccion=$("#direccion").val();
+    var telefono=$("#telefono").val();
+    var correo=$("#correo").val();
+
+    alert(nombre+direccion+telefono+correo);
+
+    $.post("registrarcliente.php",{"nombre":nombre,"direccion":direccion,"telefono":telefono,"correo":correo},function(respuesta){
+        alert(respuesta);
+        $("#nombre").val("");
+        $("#direccion").val("");
+        $("#telefono").val("");
+        $("#correo").val("");
+    });
+}
 
