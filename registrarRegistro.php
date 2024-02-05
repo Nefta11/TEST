88371ae04919ca3c8ec2bbf0a51c958fc2f4eb9c
Nefta11 <neftaliarturohernandez@gmail.com>
2024-02-05 16:40:29 -0600

    Update

--- registrarRegistro.php
+++ registrarRegistro.php
@@ -1,12 +1,22 @@
 <?php
 include 'conexion.php';
-$idVehiculo=$_POST['id_vehiculo'];
-$idCajon=$_POST['id_Cajon'];
-$idtarifa=$_POST['id_tarifa'];
-$id_encargado=$_POST['id_empleado'];
-date_default_timezone_set("America/Mexico_city");
-$
 
+$idVehiculo = $_POST['id_vehiculo'];
+$idCajon = $_POST['id_cajon'];
+$idTarifa = $_POST['id_tarifa'];
+$idEncargado = $_POST['id_empleado'];
 
+// Obtener la fecha y hora actual
+$fechaIngreso = date("Y-m-d");
+$horaIngreso = date("H:i:s");
 
+// Consulta para insertar el registro
+$query = "INSERT INTO registro (id_vehiculo, id_cajon, id_tarifa, id_empleado, fechaIngreso, horaIngreso) 
+          VALUES ('$idVehiculo', '$idCajon', '$idTarifa', '$idEncargado', '$fechaIngreso', '$horaIngreso')";
+
+if ($datos = mysqli_query($conexion, $query)) {
+    echo "Datos guardados correctamente";
+} else {
+    echo "Error, no se guardó correctamente";
+}
 ?>
