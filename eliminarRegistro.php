7affc146d637d6814fd9bf6ca700f615f84fc8e9
Nefta11 <neftaliarturohernandez@gmail.com>
2024-02-13 10:54:53 -0600

    Updated

--- eliminarRegistro.php
+++ eliminarRegistro.php
@@ -3,11 +3,20 @@ include 'conexion.php';
 
 $id_registro = $_POST['id_registro'];
 
-$stmt = $conexion->prepare("DELETE FROM registro WHERE id_registro='".$id_registro."'");
-// $stmt->bind_param("ss", $tarifa, $monto);
-$stmt2 = $conexion->prepare ("UPDATE cajon SET status = 0 WHERE id_cajon = (SELECT id_cajon FROM registro WHERE id_registro = '".$id_registro."')");
-$result2 = $stmt2->execute();
+date_default_timezone_set('America/Mexico_City');  
+$fecha_salida = date("Y-m-d");
+$hora_salida = date("H:i:s");
+
+$stmt = $conexion->prepare("UPDATE registro SET fechasalida=?, horasalida=? WHERE id_registro=?");
+$stmt->bind_param("sss", $fecha_salida, $hora_salida, $id_registro);
+
+$stmt2 = $conexion->prepare("UPDATE cajon SET status = 0 WHERE id_cajon = (SELECT id_cajon FROM registro WHERE id_registro = ?)");
+$stmt2->bind_param("s", $id_registro);
+
+// Ejecutar ambas consultas
 $result = $stmt->execute();
+$result2 = $stmt2->execute();
+
 if ($result && $result2) {
     echo "success";
 } else {
@@ -15,6 +24,6 @@ if ($result && $result2) {
 }
 
 $stmt->close();
+$stmt2->close();
 $conexion->close();
-
 ?>
